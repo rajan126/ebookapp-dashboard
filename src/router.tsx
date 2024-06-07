7e569b23fd83864f2df90a303e3d9e37eba166df
rajansingh126 <rajansingh895876@gmail.com>
2024-06-07 11:22:00 +0530

    Dashboard layout

--- src/router.tsx
+++ src/router.tsx
@@ -3,13 +3,27 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import LoginPage from "@/pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import DeshboardLayout from "./layouts/DeshboardLayout";
+import BookPage from "./pages/BookPage";
+
 
 const router = createBrowserRouter(
   [
     {
-        path:'/',
-        element:<HomePage/>
-    }, 
+      path: 'dashboard',
+      element: <DeshboardLayout />,
+      children: [
+          {
+              path: 'home',
+              element: <HomePage />,
+          },
+          {
+            path: 'books',
+            element: <BookPage />,
+        },
+        
+      ],
+  },
     {
         path:'/login',
         element:<LoginPage/>
